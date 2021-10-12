a760f44843e99c5d958c0fdf1265b437261af2b7
Juanito Ortega <juanito.ortega@nivelics.co>
2021-10-11 23:18:21 -0400

    Add services

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,59 +14,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 1',
-      price: 1000
-    },
-    {
-      name: 'Producto 2',
-      price: 2000
-    },
-    {
-      name: 'Producto 3',
-      price: 3000
-    },
-    {
-      name: 'Producto 4',
-      price: 4000
-    },
-    {
-      name: 'Producto 5',
-      price: 5000
-    }
-  ]);
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Producto 1',
-    price: 1000
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parámetros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
